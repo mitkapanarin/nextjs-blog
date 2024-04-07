39401ceafaabc6786a1ed03742436e72fdaffe1c
mitkapanarin <mitka_j@yahoo.com>
2024-04-07 15:39:08 +0200

    Blog Content added

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -1,9 +1,22 @@
+import BlogContent from "@/components/BlogContent";
 import Hero from "@/components/Hero";
+import { client } from "../../../sanity/lib/client";
+import { groq } from "next-sanity";
 
-export default function Home() {
+export const revalidate = 30;
+const query = groq`*[_type == 'post']{
+  ...,
+  author->,
+  categories[]->
+} | order(_createdAt asc)`;
+
+export default async function Home() {
+  const posts = await client.fetch(query);
+
   return (
     <main>
       <Hero />
+      <BlogContent posts={posts} />
     </main>
   );
 }
